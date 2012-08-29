0c0ced61b98c8905b72be45d450c954ea831adda
kucaahbe <kucaahbe@ukr.net>
2012-08-29 13:28:43 +0300

    added rbenv install

--- install.sh
+++ install.sh
@@ -1,3 +1,27 @@
 #!/bin/sh
 
 ln -sf $PWD/gemrc ~/.gemrc
+
+RBENV_URL='git://github.com/sstephenson/rbenv.git'
+RBENV_GEMSET_URL='git://github.com/jamis/rbenv-gemset.git'
+RUBY_BUILD_URL='git://github.com/sstephenson/ruby-build.git'
+
+GIT_CLONE='git clone --'
+
+rm -rf $HOME/.rbenv
+
+echo installing rbenv
+$GIT_CLONE $RBENV_URL         $HOME/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> $HOME/.zshenv.local
+echo 'eval "$(rbenv init -)"'               >> $HOME/.zshenv.local
+mkdir -p $HOME/.rbenv/plugins
+
+sleep 1
+echo installing rbenv-gemset
+$GIT_CLONE $RBENV_GEMSET_URL  $HOME/.rbenv/plugins/rbenv-gemset
+
+sleep 1
+echo installing ruby-build
+$GIT_CLONE $RUBY_BUILD_URL  $HOME/.rbenv/plugins/ruby-build
+
+echo restart $SHELL now
